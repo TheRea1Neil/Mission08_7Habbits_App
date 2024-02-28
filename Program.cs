338eee31a617d837de0f14db871160ba406e6b82
afoote99 <afoote99@gmail.com>
2024-02-28 09:19:17 -0700

    added endpoints in appsettings and Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mission08_7Habbits_App.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<HighHabitsContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:HabitsConnection"]);
+});
 
 var app = builder.Build();
 
