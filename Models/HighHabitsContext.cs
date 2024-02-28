0bede9898b5165bf0bbda572f2b22fb7ff7cf216
afoote99 <afoote99@gmail.com>
2024-02-28 09:06:59 -0700

    Added Database and models

---
+++ Models/HighHabitsContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_7Habbits_App.Models;
+
+public partial class HighHabitsContext : DbContext
+{
+    public HighHabitsContext()
+    {
+    }
+
+    public HighHabitsContext(DbContextOptions<HighHabitsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=HighHabits.sqlite");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.Property(e => e.CategoryId)
+                .ValueGeneratedNever()
+                .HasColumnName("CategoryID");
+        });
+
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.Property(e => e.TaskId)
+                .ValueGeneratedNever()
+                .HasColumnName("TaskID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.Completed).HasColumnType("BOOLEAN");
+            entity.Property(e => e.DueDate).HasColumnType("DATETIME");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Tasks).HasForeignKey(d => d.CategoryId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
