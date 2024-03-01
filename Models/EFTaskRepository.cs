c550bf82e989cc7dccc669e1076f567b131dc993
Neilb <neilb1999@gmail.com>
2024-02-29 19:02:35 -0700

    yes

---
+++ Models/EFTaskRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using SQLitePCL;
+
+namespace Mission08_7Habbits_App.Models
+{
+    public class EFTaskRepository : iTaskRepository
+    {
+        private HighHabitsContext _context;
+
+        public EFTaskRepository(HighHabitsContext context) 
+        {
+            _context = context;
+        }
+
+        public List<Task> Tasks => _context.Tasks.ToList();
+    }
+}
